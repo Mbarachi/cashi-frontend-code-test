fbd4d7da125907335693b854881d773d3f4d5f20
victor <39647480+Mbarachi@users.noreply.github.com>
2025-11-12 03:19:32 +0100

    feat: configure React Router and setup routes for login, dashboard, and transactions pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import TransactionDetail from "./pages/TransactionDetail";
+
 function App() {
-  return <h1>Implement code test</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/transactions/:id" element={<TransactionDetail />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
